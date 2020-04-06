01c01f23a2a92b505a8acaed78d48a83b38fd5a8
sanyam-S <49986897+sanyam-S@users.noreply.github.com>
2020-04-06 19:27:27 +0530

    Update beforeinstall.sh

--- script/beforeinstall.sh
+++ script/beforeinstall.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 if [ -d /var/www/html/humhub ];then 
       sudo rm -rf /tmp/humhub.back \
-      && sudo mv /var/www/html/humhub /tmp/humhub.back
+      sudo mv /var/www/html/humhub /tmp/humhub.back
 fi
